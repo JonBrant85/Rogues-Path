3549de5825b3f82dc79e1c3d1db64b95ff63bab7
JonBrant85 <jonbrant4@gmail.com>
2026-09-01 17:34:04 -0400

    Allow configured removal commands on empty items

--- Assets/_Rogues Path/Crafting/OrbCommands/RemoveRandomModifier.cs
+++ Assets/_Rogues Path/Crafting/OrbCommands/RemoveRandomModifier.cs
@@ -3,13 +3,19 @@ using System;
 namespace _Rogues_Path.Crafting.Commands {
     [Serializable]
     public class RemoveRandomModifier : OrbCommand {
+        public bool AllowEmpty;
+
         public override bool Execute(
             OrbCommandContext context) {
 
-            if (context?.Equipment?.CraftedModifiers == null ||
-                context.Equipment.CraftedModifiers.Count == 0) {
+            if (context?.Equipment?.CraftedModifiers == null) {
 
                 return false;
+            }
+
+            if (context.Equipment.CraftedModifiers.Count == 0) {
+
+                return AllowEmpty;
             }
 
             int index = UnityEngine.Random.Range(
